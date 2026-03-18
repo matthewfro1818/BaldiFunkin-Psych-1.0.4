30cae076c11889d6a59b096ddf4e834156c3280d
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2026-03-18 06:20:22 -0400

    Update windows.bat

--- setup/windows.bat
+++ setup/windows.bat
@@ -18,7 +18,7 @@ haxelib set lime 8.1.2
 haxelib set openfl 9.3.3
 haxelib install away3d
 haxelib install hxIni
-haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate\
+haxelib git flxanimate https://github.com/Dot-Stuff/flxanimate
 haxelib git linc_luajit https://github.com/superpowers04/linc_luajit 1906c4a96f6bb6df66562b3f24c62f4c5bba14a7
 haxelib git funkin.vis https://github.com/FunkinCrew/funkVis 22b1ce089dd924f15cdc4632397ef3504d464e90
 haxelib git grig.audio https://gitlab.com/haxe-grig/grig.audio.git cbf91e2180fd2e374924fe74844086aab7891666
